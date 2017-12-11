43e317927fa642323db46c0518b666e8a746d8a5
nekobato <nekobato@gmail.com>
2017-12-12 02:15:33 +0900

    get active tab's title&url

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <a @click="getInfo">getPageInfo</a>
+    <span>{{ info }}</span>
   </div>
 </template>
 
@@ -24,7 +10,14 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      info: 'hoge'
+    }
+  },
+  methods: {
+    getInfo () {
+      chrome.tabs.getSelected(null, (tab) => {
+          this.$data.info = `[${tab.url} ${tab.title}]`
+      })
     }
   }
 }
@@ -37,21 +30,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
+  padding: 16px;
+  width: 200px;
+  height: 32px;
 }
 
 a {
